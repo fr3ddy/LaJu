ce319d18cafae484dc8c94ddbf8ea853f3578462
Freddy <spamdomain@gmx.net>
2015-05-17 18:18:12 +0200

    changes

--- app/src/main/java/de/dhbw_loerrach/laju/InfoTabFragment.java
+++ app/src/main/java/de/dhbw_loerrach/laju/InfoTabFragment.java
@@ -1,18 +1,17 @@
 package de.dhbw_loerrach.laju;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.apache.http.NameValuePair;
-import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,10 +19,7 @@ import java.util.List;
  * Created by Frederik on 16.05.2015.
  */
 public class InfoTabFragment extends Fragment {
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
+
     public static InfoTabFragment newInstance() {
         InfoTabFragment fragment = new InfoTabFragment();
         Bundle args = new Bundle();
@@ -38,7 +34,7 @@ public class InfoTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_infotab, container, false);
         // Building Parameters ( you can pass as many parameters as you want)
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        //List<NameValuePair> params = new ArrayList<NameValuePair>();
         //params.add(new BasicNameValuePair("appkey", "123456"));
         // Getting JSON Object
         // http://laju.frederik-frey.de/lajuapp/alleNews/123456
